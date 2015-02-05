4bbc20a10f03c2fd15454ddeb8e79f5482ca9120
vladiperezrodriguez@gmail.com <vladiperezrodriguez@gmail.com>
2015-02-05 19:13:08 +0100

    enlace a noticias

--- application/views/noticias/ver.php
+++ application/views/noticias/ver.php
@@ -6,6 +6,9 @@
     <h1><?= $cabecera ?></h1>
     <p><?= $texto_noticia ?></p>
     <p>Fecha de la noticia: <?= $fecha ?></p>
+
+    <div><a href="/noticias/index.php">Volver</a></div>
+
   </body>
 </html>
 
