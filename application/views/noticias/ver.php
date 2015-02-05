c4bab44c83513d5a187c5ec77c3e941eb7de03e6
vladiperezrodriguez@gmail.com <vladiperezrodriguez@gmail.com>
2015-02-05 19:02:30 +0100

    noticias vr

---
+++ application/views/noticias/ver.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <h1><?= $cabecera ?></h1>
+    <p><?= $texto_noticia ?></p>
+    <p>Fecha de la noticia: <?= $fecha ?></p>
+  </body>
+</html>
+
