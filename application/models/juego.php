b7d1a2556615ff77a8227f6ed3f07752850da52a
M. A. Glez. Souto <miguelangel.gonzalez.souto@iesdonana.org>
2015-02-09 20:44:27 +0100

    corregido error sintactico en el modelo juego

--- application/models/juego.php
+++ application/models/juego.php
@@ -4,7 +4,6 @@ class Juego extends CI_Model
 {
     public function juego_por_id($id)
     {
-
     	$res = $this->db->query("select j.id, j.titulo, j.caratula, j.descripcion, j.fecha_lanzamiento, j.precio, d.nombre_desarrollador from juegos j, desarrolladores d where j.id=d.id and j.id = ?", array($id));
                               
         return ($res->num_rows() > 0) ? $res->row_array() : FALSE;
@@ -12,15 +11,14 @@ class Juego extends CI_Model
 
     public function sistema_operativo_por_id_juego($id)
     {
-
-
     	$res = $this->db->query("select so.nombre_so from juegos j, juegos_so js, sistemas_operativos so where j.id=js.juegos_id and js.so_id=so.id and j.id = ?", array($id));
   
                                       
         return ($res->num_rows() > 0) ? $res->result_array() : FALSE;
     }  
 
-    public function juegos_segun_nombre($nombre){
+    public function juegos_segun_nombre($nombre)
+    {
       $nombre = '%'.$nombre.'%';      
       $res = $this->db->query("select *
                                  from juegos
@@ -32,6 +30,6 @@ class Juego extends CI_Model
     public function destacados()
     {
         return $this->db->from('juegos')->where('destacado','true')
-        ->get()->result_array());
+        ->get()->result_array();
     }
 }
