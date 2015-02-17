2dbdcbeee538b3808462283ef7165038879f8c35
M. A. Glez. Souto <miguelangel.gonzalez.souto@iesdonana.org>
2015-02-17 20:49:54 +0100

    estilo para la plantilla común

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,26 +1,44 @@
-<h2>Juegos destacados</h2>
-
-<?php foreach ($juegos as $juego):?>
-    <img src="<?= $juego['url'] ?>">
-    <h3><?= $juego['titulo'] ?></h3>
-    <p><?= $juego['descripcion'] ?></p>
-    <p><?= $juego['precio'] ?></p>
-<?php endforeach;?>
-
-<h2>Últimas noticias</h2>
-<?php foreach ($noticias as $noticia):?>
-    <h3><?= $noticia['cabecera'] ?></h3>
-    <p><?= $noticia['texto_noticia'] ?></p>
-    <p><?= $noticia['fecha'] ?></p>
-<?php endforeach;?>
-
-<h2>Mi biblioteca</h2>
-<?php if(isset($usuario_id)): ?>
-    <?php foreach ($biblioteca as $mijuego):?>
-        <img src="<?= $mijuego['url'] ?>">
-        <h3><?= $mijuego['titulo'] ?></h3>
-        <p><?= $mijuego['descripcion'] ?></p>
+<section class="cajones">
+    <h2>Juegos destacados</h2>
+    <?php foreach ($juegos as $juego):?>
+        <article class="lista">
+            <img src="<?= $juego['url'] ?>">
+            <div class="info">
+                <h3><?= $juego['titulo'] ?></h3>
+                <p><?= $juego['descripcion'] ?></p>
+                <div class="precio"><?= $juego['precio'] ?></div>
+            </div>
+        </article>
     <?php endforeach;?>
-<?php else: ?>
-    <?= anchor('/usuarios/login', 'Log in...') ?>
-<?php endif; ?>
+</section>
+
+<section class="cajones">
+    <h2>Últimas noticias</h2>
+    <?php foreach ($noticias as $noticia):?>
+        <article class="listanoticias">
+            <h3><?= $noticia['cabecera'] ?></h3>
+            <p><?= $noticia['texto_noticia'] ?></p>
+            <?= anchor('noticias/ver/'.$noticia['id'],'Ver más...')?>
+            <p><?= $noticia['fecha'] ?></p>
+        </article>
+    <?php endforeach;?>
+</section>
+
+<section class="cajones">
+<h2>Mi biblioteca</h2>
+    <?php if(isset($usuario_id)): ?>
+        <?php foreach ($biblioteca as $mijuego):?>
+            <article class="lista">
+                <img src="<?= $mijuego['url'] ?>">
+                <div class="info">
+                    <h3><?= $mijuego['titulo'] ?></h3>
+                    <p><?= $mijuego['descripcion'] ?></p>
+                </div>
+            </article>
+        <?php endforeach;?>
+    <?php else: ?>
+        <article>
+            <?= anchor('/usuarios/login', 'Log in...') ?>
+        </article>
+    <?php endif; ?>
+</section>
--- application/views/plantillas/comun.php
+++ application/views/plantillas/comun.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title><?=$titulo?></title>
+    <link rel="stylesheet" type="text/css" href="/ci_steam/css/estilo.css">
 </head>
 <body>
     <header>
         <h1>STIM</h1>
-        <?php if(isset($usuario)): ?>
-            <p>usuario: <span><?=$usuario?></span></p>
-            <button><?=anchor('/usuarios/logout', 'Log Out')?></button>
-        <?php else: ?>
-            <button><?=anchor('/usuarios/login', 'Log in')?></button>
-            <button><?=anchor('/usuarios/singup', 'Sign Up')?></button>
-        <?php endif; ?>
+        <div>
+            <?php if(isset($usuario)): ?>
+                <p>usuario: <span><?=$usuario?></span></p>
+                <button><?=anchor('/usuarios/logout', 'Log Out')?></button>
+            <?php else: ?>
+                <button><?=anchor('/usuarios/login', 'Log in')?></button>
+                <button><?=anchor('/usuarios/singup', 'Sign Up')?></button>
+            <?php endif; ?>
+        </div>
     </header>
     
     <?= $contents ?>
