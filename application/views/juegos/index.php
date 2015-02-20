4e4baa8abee1c9bfe4315afbc495fd34b620a923
Isaac Naval <holaisaac@gmail.com>
2015-02-20 15:24:57 +0100

    Tengo que hacer un push

--- application/views/juegos/index.php
+++ application/views/juegos/index.php
@@ -30,11 +30,30 @@
 				<span><?= $genero['nombre_genero'] ?></span>
 			<?php endforeach;?>
 		</p>
+		
+		<?php 
+		//echo $juego['id'];
+		//var_dump($comentarios[$juego['id']-1][0]['juegos_id']);
+		//var_dump($comentarios[$juego['id']-1][$fila]['texto_comentario']);
+		?>
 		<p>Comentarios: 
 			<!--rellena todos los comentarios para un juego-->
-			<?php foreach ($comentarios[$fila] as $comentario):?>
-				<span><?= $comentario['juegos_id'] ?></span>
-				<span><?= $comentario['texto_comentario'] ?></span>
+			<?php $con=0; ?>
+			<?php foreach ($comentarios as $comentario):?>
+				<?php
+				//var_dump($comentario['texto_comentario']);
+				echo "Texto comentario".$comentarios[$juego['id']-1][$con]['texto_comentario']."<br/>";
+				echo "contador".$con."<br/>";
+				echo "juego[id]".$juego['id']."<br/>";
+				echo "Juegos_id".$comentarios[$juego['id']-1][$con]['juegos_id']."<br/>";
+				echo isset($comentarios[$juego['id']-1][$con]['juegos_id']);
+				?>
+				<?php if ($juego['id']==$comentarios[$juego['id']-1][$con]['juegos_id'] &&
+							isset($comentarios[$juego['id']-1][$con]['juegos_id'])==TRUE): ?>
+					<span><?= $comentarios[$juego['id']-1][$con] ?></span>
+				<?php endif ?>
+				
+			<?php $con++; ?>	
 			<?php endforeach;?>
 		</p>
 
